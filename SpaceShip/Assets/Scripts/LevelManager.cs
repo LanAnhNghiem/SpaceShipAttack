96db66b9912b0a49a42760a6f5d14d0d34b8862c
Phate Opera <14520560@gm.uit.edu.vn>
2017-06-29 11:32:23 +0700

    Create Basic Menu Scene
    Create a basic menu scene, included:
    - Buttons
    - Game title & information
    - Script for scene switching
    - Others (such as fonts, prefabs, UI images)

---
+++ SpaceShip/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+	private void LoadScene(int idx)
+	{
+		SceneManager.LoadScene(idx);
+	}
+
+	public void LoadNextScene()
+	{
+		int currIndex = SceneManager.GetActiveScene ().buildIndex;
+		LoadScene(currIndex + 1);
+	}
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
