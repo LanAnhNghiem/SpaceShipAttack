617270ef67fed5da666c238db20becebf3f7a553
Phate Opera <14520560@gm.uit.edu.vn>
2017-07-01 11:06:19 +0700

    Update menu scene
    Update menu scene, include:
    - Add music
    - Add instructions canvas & animations between instructions and main
    menu canvas

---
+++ SpaceShip/Assets/Scripts/CanvasAnimationController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasAnimationController : MonoBehaviour {
+
+    // Use this for initialization
+    private Animator canvasAnimator;
+
+	void Start () {
+        canvasAnimator = GetComponent<Animator>();
+	}
+	
+	public void ShowInstruction()
+    {
+        canvasAnimator.SetBool("IsInstructionsShow", true);
+    }
+
+    public void ReturnToMainMenu()
+    {
+        canvasAnimator.SetBool("IsInstructionsShow", false);
+    }
+}
