ae85087ed73b4cd8b6370d46aca172d1ae453584
Phate Opera <14520560@gm.uit.edu.vn>
2017-07-08 15:38:22 +0700

    Update (missing script from commit cc9f5d5)

---
+++ SpaceShip/Assets/_Scripts/ScoreDetection.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreDetection : MonoBehaviour {
+
+    public int bonusScoreMode = 0;
+
+    void OnTriggerEnter(Collider otherCollider)
+    {
+        if (true)
+        {
+            ScoreKeeper scoreKeeper = FindObjectOfType<ScoreKeeper>();
+            scoreKeeper.IncrementScore(bonusScoreMode);
+        }
+    }
+}
---
+++ SpaceShip/Assets/_Scripts/ScoreKeeper.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreKeeper : MonoBehaviour {
+
+    int score = 0;
+    public int[] ScoreModes;
+    public Text scoreText;
+
+    // Use this for initialization
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.text = "Score: " + score;
+    }
+
+    public void IncrementScore(int bonusScoreMode)
+    {
+        print("Detect Score");
+        if (bonusScoreMode < 0 && bonusScoreMode > ScoreModes.Length - 1)
+        {
+            return;
+        }
+        score += ScoreModes[bonusScoreMode];
+        //AudioSource audio = GetComponent<AudioSource>();
+        //audio.Play();
+    }
+}
